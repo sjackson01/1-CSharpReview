5911d46f5815f51fa4b4915f5fdffaee9daeef39
sjackson01 <stevenpjackson0@gmail.com>
2020-03-16 14:55:50 -0500

    Add (program.cs>Hunk 1) : Create basic var display script.

--- 1-CSharpReview/Program.cs
+++ 1-CSharpReview/Program.cs
@@ -5,8 +5,18 @@ namespace _1_CSharpReview
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            // Create vars
+            var number = 4;
+            var greeting = "Hello";
+
+            // Display and manipulate vars
+            Console.WriteLine(number);
+            Console.WriteLine(greeting);
+            Console.WriteLine(number + 2);
+            Console.WriteLine(12 - number);
+
+            Console.ReadLine();
         }
     }
 }
