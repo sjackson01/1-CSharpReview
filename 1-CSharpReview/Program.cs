b602487665127c1143464c3a2162bfb4260f0e0e
sjackson01 <stevenpjackson0@gmail.com>
2020-03-17 10:40:23 -0500

    Modify (program.cs>Hunk 1) : Update comment. Add (program.cs>Hunk 2) : Demo default types.

--- 1-CSharpReview/Program.cs
+++ 1-CSharpReview/Program.cs
@@ -6,7 +6,7 @@ namespace _1_CSharpReview
     {
         static void Main(string[] args)
         {   
-            // Create vars
+            // Create vars declaration
             var number = 4;
             var greeting = "Hello";
 
@@ -15,6 +15,17 @@ namespace _1_CSharpReview
             Console.WriteLine(greeting);
             Console.WriteLine(number + 2);
             Console.WriteLine(12 - number);
+
+            // Re-assign value to var assignment
+            number = 10;
+
+            Console.WriteLine(12 - number);
+
+            Console.WriteLine(1.GetType());         // System.Int32
+            Console.WriteLine(3.14.GetType());      // System.Double
+            Console.WriteLine("hello".GetType());   // System.String
+            Console.WriteLine(true.GetType());      // System.Boolean
+            Console.WriteLine('z'.GetType());       // System.Char
 
             Console.ReadLine();
         }
